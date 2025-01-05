be703344f81048cd9b1d48da6708aa42fb881c59
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-01-05 08:56:02 +0500

    component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
-}
+const App = () => {
+   return (
+      <div className="App">
+         Hello, samurai! Let's go!
+         <Header />
+         <Technologies />
+      </div>
+   );
+};
+
+const Header = () => {
+   return (
+      <div>
+         <a href="#">Home </a>
+         <a href="#">New Feed </a>
+         <a href="#">Messages </a>
+      </div>
+   );
+};
+const Technologies = () => {
+   return (
+      <ul>
+         <li>CSS</li>
+         <li>Html</li>
+         <li>js</li>
+         <li>REACT</li>
+      </ul>
+   );
+};
 
 export default App;
