68f1b253ffb3376657bdd5622e3e5e4a2ff37c91
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-01-16 10:35:49 +0500

    create module css styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-
+import s from './NavBar.module.css';
 export const NavBar = () => {
    return (
-      <nav className="nav">
+      <nav className={s.nav}>
          <ul>
-            <li>
+            <li className={s.item}>
                <a href="#">Profile</a>
             </li>
-            <li>
+            <li className={s.item}>
                <a href="#">Messages</a>
             </li>
-            <li>
+            <li className={s.item}>
                <a href="#">News</a>
             </li>
-            <li>
+            <li className={s.item}>
                <a href="#">Music</a>
             </li>
-            <li>
+            <li className={s.item}>
                <a href="#">Setting</a>
             </li>
          </ul>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import s from './Profile.module.css';
 export const Profile = () => {
    return (
-      <div className="content">
+      <div className={s.content}>
          <div>
             <img
                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi7ZM5IRn9GL1_cdI8TDtkdKFx_9YVRBOUNw&s"
@@ -18,9 +18,9 @@ export const Profile = () => {
          <div>
             My posts
             <div>New posts</div>
-            <div>
-               <div>Post 1</div>
-               <div>Post 2</div>
+            <div className={s.posts}>
+               <div className={s.item}>Post 1</div>
+               <div className={s.item}>Post 2</div>
             </div>
          </div>
       </div>
