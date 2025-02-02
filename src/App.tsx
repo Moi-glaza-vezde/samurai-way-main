b3e3df72b166bd64573b7dffaf8c966dd75f164b
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-02-02 18:43:25 +0500

    create navLink and style class activLink

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import {NavBar} from './components/navBar/NavBar';
 import {Profile} from './components/profile/Profile';
 import {Dialogs} from './components/dialogs/Dialogs';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {Music} from './components/music/Music';
+import {News} from './components/news/News';
+import {Setting} from './components/setting/Setting';
 
 const App = () => {
     return (
@@ -15,6 +18,9 @@ const App = () => {
                 <div className="app-wrapper-content ">
                     <Route path={"/dialogs"} component={Dialogs}/>
                     <Route path={"/profile"} component={Profile}/>
+                    <Route path={"/music"} component={Music}/>
+                    <Route path={"/news"} component={News}/>
+                    <Route path={"/setting"} component={Setting}/>
                 </div>
 
             </div>
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import s from './NavBar.module.css';
-
+import {NavLink} from 'react-router-dom';
 
 
 export const NavBar = () => {
-   return (
-      <nav className={s.nav}>
-         <ul>
-            <li className={s.item}>
-               <a href="/profile">Profile</a>
-            </li>
-            <li className={s.item}>
-               <a href="/dialogs">Messages</a>
-            </li>
-            <li className={s.item}>
-               <a href="#">News</a>
-            </li>
-            <li className={s.item}>
-               <a href="#">Music</a>
-            </li>
-            <li className={s.item}>
-               <a href="#">Setting</a>
-            </li>
-         </ul>
-      </nav>
-   );
+    return (
+        <nav className={s.nav}>
+            <ul>
+                <li className={s.item}>
+                    <NavLink activeClassName={s.activeLink} to="/profile">Profile</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink activeClassName={s.activeLink} to="/dialogs">Messages</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink activeClassName={s.activeLink} to="/news">News</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink activeClassName={s.activeLink} to="/music">Music</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink activeClassName={s.activeLink} to="/setting">Setting</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 };
