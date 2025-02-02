345cc734318f9b4003ff85edf86a8efaec6fbc68
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-02-02 15:09:17 +0500

    done route

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import './App.css';
-import { Header } from './components/header/Header';
-import { NavBar } from './components/navBar/NavBar';
-import { Profile } from './components/profile/Profile';
-import { Dialogs } from './components/dialogs/Dialogs';
+import {Header} from './components/header/Header';
+import {NavBar} from './components/navBar/NavBar';
+import {Profile} from './components/profile/Profile';
+import {Dialogs} from './components/dialogs/Dialogs';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 const App = () => {
-   return (
-      <div className="app-wrapper">
-         <Header />
-         <NavBar />
-         <div className="app-wrapper-content ">
-            <Dialogs />
-            {/* <Profile /> */}
-         </div>
-      </div>
-   );
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <NavBar/>
+                <div className="app-wrapper-content ">
+                    <Route path={"/dialogs"} component={Dialogs}/>
+                    <Route path={"/profile"} component={Profile}/>
+                </div>
+
+            </div>
+        </BrowserRouter>
+    )
+        ;
 };
 
 export default App;
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import s from './NavBar.module.css';
+
+
+
 export const NavBar = () => {
    return (
       <nav className={s.nav}>
          <ul>
             <li className={s.item}>
-               <a href="#">Profile</a>
+               <a href="/profile">Profile</a>
             </li>
             <li className={s.item}>
-               <a href="#">Messages</a>
+               <a href="/dialogs">Messages</a>
             </li>
             <li className={s.item}>
                <a href="#">News</a>
